842a02bca910ec4bddcf342765812172b36aa08b
Erwin Kroon <erwin+git@k3n.nl>
2017-12-05 19:58:37 +0100

    speed up day5 a little bit

--- src/adventofcode/day5.clj
+++ src/adventofcode/day5.clj
@@ -1,5 +1,8 @@
 (ns adventofcode.day5)
 
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
+
 (defn input []
   (clojure.java.io/reader (clojure.java.io/resource "day5/input")))
 
@@ -9,33 +12,28 @@
 (defn str->number [str]
   (Integer/parseInt str))
 
-(defn calculate-next [index state update-fn]
-  (if (>= index (count state))(println [index (count state)]))
-  (let [next-index (+ index (nth state index))
-        next-state  (update state index update-fn)]
-    [next-index next-state]))
+(defn calculate-next ^long [update-fn ^long index ^longs state]
+  (let [next-index (+ index (aget state index))
+        value      (update-fn (aget state index))]
+    (aset state index ^int value)
+    next-index))
 
 (defn solve [reader update-fn]
-  (println "------")
-  (let [state (mapv str->number (line-seq reader))
-        exit  (count state)]
-    (println exit)
+  (let [^longs state (long-array (mapv str->number (line-seq reader)))
+        exit         ^int (alength state)]
     (loop [index 0
-           state state
            steps 0]
-      (let [[next-index next-state] (calculate-next index state update-fn)]
-        (if (and (< steps 30000000) (< next-index exit))
-          (do
-            (when (= 0 (mod steps 1000000)) (println "steps: " steps))
-            (recur next-index next-state (inc steps)))
+      (let [next-index (calculate-next update-fn index state)]
+        (if (< next-index exit)
+          (recur next-index (inc steps))
           (inc steps))))))
 
 (defn solve-1 [reader]
   (solve reader inc))
 
 (defn solve-2 [reader]
-  (solve reader (fn [v] (if (>= v 3) (dec v) (inc v)))))
+  (solve reader (fn [^long v] (if (>= v 3) (dec v) (inc v)))))
 
 (defn -main [& args]
-  (println "part1: " (solve-1 (input)))
-  (println "part2: " (solve-2 (input))))
+  (time (println "part1: " (solve-1 (input))))
+  (time (println "part2: " (solve-2 (input)))))

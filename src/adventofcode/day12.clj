9bdd5a2009eb83a0b8a848d5bdfee95be2ca7760
Erwin Kroon <erwin@k3n.nl>
2017-12-14 07:10:43 +0100

    day14: the ugly part2

--- src/adventofcode/day12.clj
+++ src/adventofcode/day12.clj
@@ -1,5 +1,6 @@
 (ns adventofcode.day12
   (:require [clojure.java.io :as io]
+            [clojure.set :as cs]
             [clojure.string :as str]))
 
 (def input (-> "day12/input" io/resource slurp (str/split #"\n")))
@@ -13,48 +14,50 @@
   (reduce (fn [r line] (let [[k v] (parse-line line)]
                          (assoc r k v))) {} lines))
 
-(defn solve [lines start]
-  (let [node-map (parse-lines lines)]
-    (loop [unvisited (into #{} (keys node-map))
-           distances {start 0}
-           current   start]
-      (let [neighbours (node-map current [])
-            distance (distances current)
-            new-unvisited (disj unvisited current)]
-        (if (nil? distance)
-          distances
-          (let [new-distances
-                (reduce (fn [r v]
-                          (update r v #(if % (min % (inc distance)) (inc distance))))
-                        distances
-                        (filter new-unvisited neighbours))
-                new-current
-                (ffirst (sort-by second
-                                 (remove (comp nil? second)
-                                         (map #(vector % (new-distances %))
-                                              new-unvisited))))]
-            (recur new-unvisited
-                   new-distances
-                   new-current)))))))
+(defn solve [node-map start]
+  (loop [unvisited (into #{} (keys node-map))
+         distances {start 0}
+         current start]
+    (let [neighbours (node-map current [])
+          distance (distances current)
+          new-unvisited (disj unvisited current)]
+      (if (nil? distance)
+        distances
+        (let [new-distances
+              (reduce (fn [r v]
+                        (update r v #(if % (min % (inc distance)) (inc distance))))
+                      distances
+                      (filter new-unvisited neighbours))
+              new-current
+              (ffirst (sort-by second
+                               (remove (comp nil? second)
+                                       (map #(vector % (new-distances %))
+                                            new-unvisited))))]
+          (recur new-unvisited
+                 new-distances
+                 new-current))))))
 
-(defn solve-1 [lines]
-  (-> (solve lines 0) vals count))
+(defn solve-1 [node-map]
+  (-> (solve node-map 0) vals count))
 
-(defn solve-2 [lines]
+(defn solve-2 [node-map & [from to]]
   (let [result (reduce (fn [r v]
-                         (if ((:seen r) v)
+                         (if (or ((:seen r) v) (nil? (get node-map v)))
                            r
                            (let [new
                                  (-> r
-                                     (update :seen clojure.set/union
-                                             (into #{} (keys (solve lines v))))
+                                     (update :seen cs/union
+                                             (into #{} (keys (solve node-map v))))
                                      (update :groups inc))]
                              new)))
-                       {:seen #{}
+                       {:seen   #{}
                         :groups 0}
-                       (range (count lines)))]
+                       (if (and from to)
+                         (range from to)
+                         (range (count (keys node-map)))))]
     (:groups result)))
 
 (defn -main [& args]
-  (time (println "part1: " (solve-1 input)))
-  (time (println "part2: " (solve-2 input))))
+  (let [node-map (parse-lines input)]
+    (time (println "part1: " (solve-1 node-map)))
+    (time (println "part2: " (solve-2 node-map)))))
--- src/adventofcode/day14.clj
+++ src/adventofcode/day14.clj
@@ -1,19 +1,59 @@
 (ns adventofcode.day14
-  (:require [adventofcode.day10 :refer [solve parse-2 result-2]]))
+  (:require [clojure.string :as str]
+            [adventofcode.day10 :refer [solve parse-2 result-2]]
+            [adventofcode.day12 :refer [solve-2]]))
 
-(def input (mapv #(str "jxqlasbh" "-" %) (range 128)))
+(def results
+  (delay
+    (def input (mapv #(str "jxqlasbh" "-" %) (range 128)))
 
-(defn knot-hash [input] (solve parse-2 result-2 256 input 64))
+    (defn knot-hash [input] (solve parse-2 result-2 256 input 64))
 
-(def hashes (mapv knot-hash input))
+    (def hashes (mapv knot-hash input))
 
-(defn hex->binary-vec [c]
-  (Integer/toBinaryString (Integer/parseInt (str c) 16)))
+    (defn hex->binary-vec [c]
+      (str/replace (format "%4s" (Integer/toBinaryString (Integer/parseInt (str c) 16)))
+                   #" " "0"))
 
-(defn count-1 [hex-string]
-  (reduce + (map #(-> (hex->binary-vec %)
-                      (frequencies)
-                      (get \1 0)) hex-string)))
+    (defn count-1 [hex-string]
+      (reduce + (map #(-> (hex->binary-vec %)
+                          (frequencies)
+                          (get \1 0)) hex-string)))
 
-(def part1 (reduce + (map count-1 hashes)))
+    (def part1 (reduce + (map count-1 hashes)))
 
+    (println "part1:" part1)
+
+    (def binary-vecs
+      (mapv
+        (fn [s] (mapv #(if (= \1 %) 1 0) (mapcat hex->binary-vec s)))
+        hashes))
+
+    (doall (mapv #(str/replace (apply str %)
+                               #"0" " ") binary-vecs))
+
+    (defn get-around [x y]
+      [[(inc x) y]
+       [(dec x) y]
+       [x (inc y)]
+       [x (dec y)]])
+
+    (defn make-node-map [binary-vecs]
+      (let [size (count (first binary-vecs))]
+        (into (sorted-map)
+              (remove nil?
+                      (for [row (range size)
+                            col (range size)]
+                        (when-not (zero? (get-in binary-vecs [row col]))
+                          [(inc (+ (* row size) col)),
+                           (into [] (remove zero?
+                                            (map (fn [[x y]] (* (get-in binary-vecs [x y] 0)
+                                                                (inc (+ (* x size) y))))
+                                                 (get-around row col))))]))))))
+
+    (def part2 (time (solve-2 (make-node-map binary-vecs) 1 (inc (* 128 128)))))
+
+    (println "part2:" part2)))
+
+(defn -main [& args]
+  @results)

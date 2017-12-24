2656a30e11611a8291818952f8626685f0079780
Erwin Kroon <erwin+git@k3n.nl>
2017-12-24 10:30:19 +0100

    day24: part2

--- src/adventofcode/day24.clj
+++ src/adventofcode/day24.clj
@@ -12,21 +12,38 @@
 (defn matches [parts connect-with]
   (filter (fn [[from to]] (or (= connect-with from) (= connect-with to))) parts))
 
-(defn step [nodes current connected-with path]
+(defn step [compare-fn nodes current connected-with path]
   (let [new-nodes  (disj nodes current)
         connect-to (if (= connected-with (first current)) (second current) (first current))
         matching   (matches new-nodes connect-to)]
     (if (seq matching)
-      (reduce max (map #(step new-nodes % connect-to (conj path current)) matching))
-      (reduce + (map (partial reduce +) (conj path current))))))
+      (reduce
+       compare-fn
+       (map #(step compare-fn new-nodes % connect-to (conj path current)) matching))
+      [(count (conj path current))
+       (reduce + (map (partial reduce +) (conj path current)))])))
 
-(defn solve-1 [input]
+(defn solve [compare-fn input]
   (let [nodes    (into #{} input)
         starting (matches nodes 0)]
-    (reduce max (map #(step nodes % 0 []) starting))))
+    (second
+     (reduce compare-fn (map #(step compare-fn nodes % 0 []) starting)))))
 
-#_(solve-1 input)
+(defn compare-part-1
+  [[_ max-weight]
+   [_ weight]]
+  [0 (max max-weight weight)])
 
+(defn compare-part-2
+  [[max-length
+    max-weight]
+   [length
+    weight]]
+  (cond
+    (> length max-length) [length weight]
+    (= length max-length) [length (max weight max-weight)]
+    :else [max-length max-weight]))
 
 (defn -main [& args]
-  (println (time (solve-1 input))))
+  (println "part1:" (time (solve compare-part-1 input)))
+  (println "part2:" (time (solve compare-part-2 input))))
